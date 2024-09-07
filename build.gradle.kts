b39e72b7f794490f4f9c854c34fa38e11e02e78f
FreshMag <pescomagnani@gmail.com>
2024-09-07 15:18:29 +0200

    build: updated gradle configuration. Setup of main libraries, libs.versions.toml and generation of sources via Antlr

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
+    alias(libs.plugins.kotlin.jvm)
 }
 
 group = "io.github.freshmag"
@@ -16,6 +16,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
 }
--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm")
+    antlr
+    alias(libs.plugins.ktlint)
 }
 
 group = "io.github.freshmag"
@@ -9,8 +11,41 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    main {
+        java {
+            // telling that output generateGrammarSource should be part of main source set
+            // actually passed value will be equal to `outputDirectory` that we configured above
+            srcDir(tasks.generateGrammarSource)
+        }
+    }
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation(libs.kotest)
+    implementation(libs.asm)
+    antlr(libs.antlr) // use ANTLR version 4
+}
+
+ktlint {
+    filter {
+        exclude { it.file.path.contains(layout.buildDirectory.dir("generated").get().toString()) }
+    }
+}
+
+
+tasks.generateGrammarSource {
+    // set output directory to some arbitrary location in `/build` directory.
+    // by convention `/build/generated/sources/main/java/<generator name>` is often used
+    outputDirectory = file("${project.projectDir}/generated/sources/main/java/antlr")
+
+    // pass -package to make generator put code in not default space
+    arguments = listOf("-package", "io.github.freshmag.butterfool.compiler.grammar")
 }
 
 tasks.test {
